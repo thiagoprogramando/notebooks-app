15e46da2566d7d24c885b1d3a0940054fcb167fb
Thiago César <thiago.or.code@gmail.com>
2025-08-04 14:43:26 -0300

    Corrige erro de foreign key na tabela question comments

--- database/migrations/2025_07_30_232034_create_question_comments_table.php
+++ database/migrations/2025_07_30_232034_create_question_comments_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('question_comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_answer_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('user_answer_id')->nullable()->constrained('users')->onDelete('cascade');
             $table->foreignId('question_id')->constrained()->onDelete('cascade');
             $table->foreignId('comment_id')->nullable()->constrained()->onDelete('cascade');
             $table->longText('comment');
